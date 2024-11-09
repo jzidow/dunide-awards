93310344f28a17e0a8e7675127d29f5a699a66f0
jrzidow <jrzid610@gmail.com>
2024-11-09 01:31:04 -0800

    getting asyncConfig values from appliacatiopnl.yml

--- src/main/java/com/ninjaone/dundie_awards/config/AsyncConfig.java
+++ src/main/java/com/ninjaone/dundie_awards/config/AsyncConfig.java
@@ -1,20 +1,31 @@
 package com.ninjaone.dundie_awards.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @Configuration
 public class AsyncConfig {
+    @Value("${dundie.thread-pool.core-pool-size}")
+    private int corePoolSize;
 
+    @Value("${dundie.thread-pool.max-pool-size}")
+    private int maxPoolSize;
+
+    @Value("${dundie.thread-pool.queue-capacity}")
+    private int queueCapacity;
+
+    @Value("${dundie.thread-pool.thread-name-prefix}")
+    private String threadNamePrefix;
 
     @Bean(name = "customTaskExecutor")
     public ThreadPoolTaskExecutor taskExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(2);
-        executor.setMaxPoolSize(4);
-        executor.setQueueCapacity(25);
-        executor.setThreadNamePrefix("AsyncExecutor-");
+        executor.setCorePoolSize(corePoolSize);
+        executor.setMaxPoolSize(maxPoolSize);
+        executor.setQueueCapacity(queueCapacity);
+        executor.setThreadNamePrefix(threadNamePrefix);
         executor.initialize();
         return executor;
     }
