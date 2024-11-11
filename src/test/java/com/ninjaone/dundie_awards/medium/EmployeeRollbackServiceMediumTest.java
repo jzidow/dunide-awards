e1366c28fe7a43ce518847002c75c0616b069d55
jrzidow <jrzid610@gmail.com>
2024-11-10 22:02:28 -0800

    removed old imports from rollback test

--- src/test/java/com/ninjaone/dundie_awards/medium/EmployeeRollbackServiceMediumTest.java
+++ src/test/java/com/ninjaone/dundie_awards/medium/EmployeeRollbackServiceMediumTest.java
@@ -11,16 +11,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.SpyBean;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-//@DataJpaTest
 @SpringBootTest
 public class EmployeeRollbackServiceMediumTest {
 
